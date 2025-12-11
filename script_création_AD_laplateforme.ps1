7e75e8f2ede70734d2dca877c3718666834c2720
Paul-VDH <vanderhaegenpaulo@gmail.com>
2025-12-11 11:28:18 +0100

    Add files via upload

---
+++ script_création_AD_laplateforme.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest -DomainName laplateforme.io -DomainNetBIOSName LAPLATEFORME -InstallDNS:$true -SafeModeAdministratorPassword (ConvertTo-SecureString "Azerty_2025!" -AsPlainText -Force)
